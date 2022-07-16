5962756c7564a0128631ae8940e81e09127e1968
phunht <minhphu19022000@gmail.com>
2022-07-16 16:08:58 +0700

    clone public project

---
+++ telebot/credentials.py
@@ -0,0 +1,4 @@
+# noinspection SpellCheckingInspection
+bot_token = r'5227016501:AAFK82-2ErwB_NDCOmNPgOKjF988mAkIrHc'
+bot_user_name = r'ZPS_CombatBot'
+bot_url = r'https://zpscombatbot.herokuapp.com/'
